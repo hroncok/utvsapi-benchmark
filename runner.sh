baeb40cf6915213350ad0bb1aa63beb47c6d9ee1
Miro Hrončok <miro@hroncok.cz>
2016-05-05 00:40:07 +0200

    Further adjustments for the output

--- runner.sh
+++ runner.sh
@@ -6,40 +6,45 @@ output="Test results:\n"
 
 
 b() {
-    echo "STARTING IN: $1"
-    pushd "$1"
+    echo "IN: $1"
+    pushd "$1" > /dev/null
     . venv/bin/activate
     gunicorn -w 2 "$2" &
     sleep 2
+    echo GET "$3"
     H="-H "
     if [ -z "$4" ]; then
         H=""
+    else
+        echo "$4"
     fi
+    echo
     curl $H "$4" "http://localhost:8000$3" | python3 -m json.tool
     ab_out=`ab -n "$NUM" -c "$CON" $H "$4" "http://localhost:8000$3"`
     killall gunicorn
     rps=`echo "$ab_out" | grep "Requests per second"`
     crs=`echo "$ab_out" | grep "Complete requests"`
-    output="$output\n$1:\n\t$rps\n\t$crs"
+    ccrs=`echo "$ab_out" | grep "Concurrency Level"`
+    output="$output\n$1:\n\t$rps\n\t$crs\n\t$ccrs"
     deactivate
-    popd
-    echo -e "\n\n\n"
+    popd > /dev/null
+    echo -e "\n"
 }
 
 f() {
     # change this for your directories
     case "$1" in
     "d")
-        echo utvsapi-django utvsapi.wsgi
+        echo ../utvsapi-django utvsapi.wsgi
         ;;
     "e")
-        echo utvsapi-eve utvsapi.main:app
+        echo ../utvsapi-eve utvsapi.main:app
         ;;
     "r")
-        echo utvsapi-ripozo utvsapi.main:app
+        echo ../utvsapi-ripozo utvsapi.main:app
         ;;
     "s")
-        echo utvsapi-sandman utvsapi.main:app
+        echo ../utvsapi-sandman utvsapi.main:app
         ;;
     esac
 }

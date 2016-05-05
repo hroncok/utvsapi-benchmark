5bfd98c5ac004ab57944961d184a09e9ffcc86e5
Miro Hrončok <miro@hroncok.cz>
2016-05-05 15:31:02 +0200

    Censor $STUDENT in headers

--- censor.sh
+++ censor.sh
@@ -1,7 +1,9 @@
-#!/bin/bash -e
+#!/bin/bash -eu
 sed -Ei \
     -e 's/"personal_number": .*(,|$)/"personal_number": XXXXXX\1/g' \
     -e 's/personal_number=.*"/personal_number=XXXXXX"/g' \
+    -e "s/$STUDENT/XXXXXX/g" \
     logs/*.log
 
 ! grep personal_number logs/*.log | grep -v XXXXXX
+! grep "$STUDENT" logs/*.log | grep -v XXXXXX
